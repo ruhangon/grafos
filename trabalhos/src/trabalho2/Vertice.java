af7c0c7d05dc41b96178f64f3bcd3b9879f0de17
Ruhan Gonçalves <ruhan.goncalves@gmail.com>
2019-09-29 11:29:45 -0300

    Adiciona cadastro de vértice e aresta para resolução de algoritmo de Dijkstra

---
+++ trabalhos/src/trabalho2/Vertice.java
@@ -0,0 +1,15 @@
+package trabalho2;
+
+public class Vertice {
+	// nome do vértice para a listagem
+	private String nomeDoVertice;
+
+	public String getNomeDoVertice() {
+		return nomeDoVertice;
+	}
+
+	public void setNomeDoVertice(String nomeDoVertice) {
+		this.nomeDoVertice = nomeDoVertice;
+	}
+
+}
